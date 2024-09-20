09be636be90c253d03d21dab6f3dae0758429936
Luiz Henrique <131896193+Luhenrao@users.noreply.github.com>
2024-09-20 10:53:17 -0300

    Delete atividade1.aos/demo directory

--- atividade1.aos/demo/src/main/java/com/java02/demo/Exception/PersonagemNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.projeto.demo.exception;
-
-public class PersonagemNotFoundException extends Exception {
-    public PersonagemNotFoundException(String id) {
-        super("Personagem não encontrado " + id);
-    }
-}
--- atividade1.aos/demo/src/main/java/com/java02/demo/Exception/PlanetaNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.projeto.demo.exception;
-
-public class PlanetaNotFoundException extends Exception {
-    public PlanetaNotFoundException(String id) {
-        super("Planeta não encontrado " + id);
-    }
-}
